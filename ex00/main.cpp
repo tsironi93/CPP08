cb6335fec6ebd1f453b59a4dc9d31be81a965fbd
Ioannis Tsiros <itsiros@2-C-7.42heilbronn.de>
2025-09-06 09:57:30 +0200

    Last

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,6 +1,7 @@
 #include "./easyfind.hpp"
 #include <array>
 #include <deque>
+#include <exception>
 #include <forward_list>
 #include <iostream>
 #include <list>
@@ -17,72 +18,98 @@ int main() {
   std::forward_list<int> f = {13, 14, 15};
 
   //================Valid Search===================
-  auto i = easyfind(v, 1);
-  std::cout << "================Valid Search===================" << std::endl;
-  if (i != -1) {
-    std::cout << "The value in the vector container is found at the index: "
-              << i << std::endl;
+  int i;
+  try {
+    std::cout << "================Valid Search===================" << std::endl;
+    i = easyfind(v, 1);
+    if (i != -1) {
+      std::cout << "The value in the vector container is found at the index: "
+                << i << std::endl;
+    }
+
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
   }
 
-  i = easyfind(l, 5);
-  if (i != -1) {
-    std::cout << "The value in the list container is found at the index: " << i
-              << std::endl;
+  try {
+    i = easyfind(l, 5);
+    if (i != -1) {
+      std::cout << "The value in the list container is found at the index: "
+                << i << std::endl;
+    }
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
   }
 
-  i = easyfind(d, 9);
-  if (i != -1) {
-    std::cout << "The value in the deque container is found at the index: " << i
-              << std::endl;
+  try {
+    i = easyfind(d, 9);
+    if (i != -1) {
+      std::cout << "The value in the deque container is found at the index: "
+                << i << std::endl;
+    }
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
   }
 
-  i = easyfind(a, 12);
-  if (i != -1) {
-    std::cout << "The value in the array container is found at the index: " << i
-              << std::endl;
+  try {
+    i = easyfind(a, 12);
+    if (i != -1) {
+      std::cout << "The value in the array container is found at the index: "
+                << i << std::endl;
+    }
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
   }
 
-  i = easyfind(f, 13);
-  if (i != -1) {
-    std::cout
-        << "The value in the forward_list container is found at the index: "
-        << i << std::endl;
+  try {
+    i = easyfind(f, 13);
+    if (i != -1) {
+      std::cout
+          << "The value in the forward_list container is found at the index: "
+          << i << std::endl;
+    }
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
   }
   std::cout << std::endl;
 
   //===================Non Valid Search===============
-  std::cout << "================Non Valid Search==================="
-            << std::endl;
-  i = easyfind(v, 100);
-  if (i != -1) {
-    std::cout << "The value in the vector container is found at the index: "
-              << i << std::endl;
-  }
 
-  i = easyfind(l, 500);
-  if (i != -1) {
-    std::cout << "The value in the list container is found at the index: " << i
+  try {
+    std::cout << "================Non Valid Search==================="
               << std::endl;
-  }
+    i = easyfind(v, 100);
+    if (i != -1) {
+      std::cout << "The value in the vector container is found at the index: "
+                << i << std::endl;
+    }
 
-  i = easyfind(d, 900);
-  if (i != -1) {
-    std::cout << "The value in the deque container is found at the index: " << i
-              << std::endl;
-  }
+    i = easyfind(l, 500);
+    if (i != -1) {
+      std::cout << "The value in the list container is found at the index: "
+                << i << std::endl;
+    }
 
-  i = easyfind(a, 1200);
-  if (i != -1) {
-    std::cout << "The value in the array container is found at the index: " << i
-              << std::endl;
-  }
+    i = easyfind(d, 900);
+    if (i != -1) {
+      std::cout << "The value in the deque container is found at the index: "
+                << i << std::endl;
+    }
 
-  i = easyfind(f, 1300);
-  if (i != -1) {
-    std::cout
-        << "The value in the forward_list container is found at the index: "
-        << i << std::endl;
-  }
+    i = easyfind(a, 1200);
+    if (i != -1) {
+      std::cout << "The value in the array container is found at the index: "
+                << i << std::endl;
+    }
 
+    i = easyfind(f, 1300);
+    if (i != -1) {
+      std::cout
+          << "The value in the forward_list container is found at the index: "
+          << i << std::endl;
+    }
+  } catch (std::exception &e) {
+    std::cout << e.what() << std::endl;
+  }
   return 0;
 }
